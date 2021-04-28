c50b634ca5da2d96743938d789b46c720877cb7f
malakmohammad95 <malakaboowida@gmail.com>
2021-04-28 17:52:58 +0300

    second change  to give id for input

--- My Blog.js
+++ My Blog.js
@@ -4,23 +4,6 @@
 const title=$("<h1>Up Loading Image From Local Storage To My Blog </h1>")
 const body = $("body");
 title.appendTo(body);
-
-const btn1=$("<button> choose file.. </button>")
-btn1.appendTo(body);
-btn1.css({
-    "font-size": "26px",
-    color: "red",
-    "border-radius": "20%"
-  
-  });
-
-const input=$("<input>  </input>")
-input.appendTo(body);
-input.id("myFile")
-input.css({
-    "font-size": "26px",
-    "width":"150"
-  });
 //=======================================
 //selecting using js
 document.querySelector("#myFile").addEventListener("change",function(){
@@ -28,8 +11,16 @@ document.querySelector("#myFile").addEventListener("change",function(){
     reader.addEventListener("load",()=>{
      localStorage.setItem("recent-Image",reader.result);
     
-    })
-    reader.readAsDataURL(this.files[0]);
-    
+    });
+  
+        reader.readAsDataURL(this.files[0]);
     });
 
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const recentImageUrl=localStorage.getItem("recent-Image");
+
+if(recentImageUrl){
+    document.querySelector("#imgPreview").setAttribute("src",recentImageUrl);
+}
+});
